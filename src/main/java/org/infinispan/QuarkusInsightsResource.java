62f8d8bf307e4eb0b74790dd795e5ee8acc26103
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-01-20 20:06:50 +0100

    Add tracing demo changes

--- src/main/java/org/infinispan/QuarkusInsightsResource.java
+++ src/main/java/org/infinispan/QuarkusInsightsResource.java
@@ -1,13 +1,21 @@
 package org.infinispan;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.infinispan.client.hotrod.RemoteCache;
 
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.quarkus.infinispan.client.Remote;
 
 @Path("/hello")
@@ -25,4 +33,14 @@ public class QuarkusInsightsResource {
       return "Hello Quarkus";
    }
 
+   @PUT
+   @Produces(MediaType.TEXT_PLAIN)
+   @WithSpan(value = "wait-for-all-response-span", kind = SpanKind.CLIENT)
+   public List<Object> putAll() {
+      CompletableFuture[] promises = IntStream.range(0, 10).boxed()
+            .map(value -> greetingsCache.putAsync(value.toString(), Character.toString('A' + value)))
+            .toList().toArray(new CompletableFuture[0]);
+
+      return Arrays.stream(promises).map(item -> item.join()).toList();
+   }
 }
