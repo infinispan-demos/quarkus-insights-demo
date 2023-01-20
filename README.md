f617acfb1df6f00e48b1f10d43076a885704bd79
Katia Aresti <karesti@redhat.com>
2023-01-20 16:03:16 +0100

    create cache from nothing

--- README.md
+++ README.md
@@ -7,5 +7,27 @@ This project is based on the following repositories:
 
 # Steps Building
 
-1. Start Quarkus
+1. Start Quarkus with Docker running
+```bash
+./mvnw quarkus:dev
+```
+2. Open Dev UI (d), open the Infinispan Console
+3. Inject a cache in Greeting Resource
+```java
+    @Inject
+    @Remote("greetings")
+    RemoteCache<String, String> greetingsCache;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        greetingsCache.put("123", "Hello Quarkus Insights");
+        return "Hello Quarkus";
+    }
+```
+4. Command line 
+```bash
+http localhost:8080/hello 
+```
+5. 
 
--- src/main/java/org/infinispan/GreetingResource.java
+++ src/main/java/org/infinispan/GreetingResource.java
@@ -1,5 +1,9 @@
 package org.infinispan;
 
+import io.quarkus.infinispan.client.Remote;
+import org.infinispan.client.hotrod.RemoteCache;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +12,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    @Remote("greetings")
+    RemoteCache<String, String> greetingsCache;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        greetingsCache.put("123", "Hello Quarkus Insights");
+        return "Hello Quarkus";
     }
 }
