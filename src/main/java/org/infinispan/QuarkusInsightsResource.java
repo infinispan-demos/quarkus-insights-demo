f9740713dc79c091c33f550907b37ccc5665930f
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-01-23 11:22:37 +0100

    Suggestion for the middle

--- src/main/java/org/infinispan/QuarkusInsightsResource.java
+++ src/main/java/org/infinispan/QuarkusInsightsResource.java
@@ -1,14 +1,9 @@
 package org.infinispan;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -77,28 +72,4 @@ public class QuarkusInsightsResource {
    /*************************
     * TRACING async and bulk
     *************************/
-   @PUT
-   @Path("async/{calls}")
-   @Produces(MediaType.TEXT_PLAIN)
-   public String putAsync(@PathParam("calls") Integer calls) {
-      CompletableFuture[] promises = IntStream.range(0, calls).boxed()
-            .map(value -> greetingsCache.putAsync(value.toString(),
-                  Character.toString('A' + value)))
-            .toList().toArray(new CompletableFuture[0]);
-
-      CompletableFuture.allOf(promises).join();
-
-      return "Executed " + calls + " calls.";
-   }
-
-   @PUT
-   @Path("bulk/{calls}")
-   @Produces(MediaType.TEXT_PLAIN)
-   public String putAll(@PathParam("calls") Integer calls) {
-      greetingsCache.putAll(IntStream.range(0, calls).boxed()
-            .collect(Collectors.toMap(value -> value.toString(),
-                  value -> Character.toString('A' + value))));
-
-      return "Executed " + calls + " calls.";
-   }
 }
