ea8fae27ab8ba19eba8fc9c66c6c8681c57d1b3e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-01-23 11:47:56 +0100

    Other suggestion for the middle

--- src/main/java/org/infinispan/bounded/DevelopersSchema.java
+++ src/main/java/org/infinispan/bounded/DevelopersSchema.java
@@ -2,9 +2,6 @@ package org.infinispan.bounded;
 
 import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
-import org.infinispan.search.Author;
-import org.infinispan.search.Book;
-import org.infinispan.search.Review;
 
 @AutoProtoSchemaBuilder(includeClasses = { Developer.class },
       schemaFileName = "developers-schema.proto",
